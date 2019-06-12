80a949aa94423fbff49e93221ec5284736ac7bff
양효령/미디어클라우드스쿼드 <hyoryeongyang@sk.com>
2019-06-12 16:39:58 +0900

    modify

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,38 @@
 import React from "react";
+import styled from 'styled-components';
 import {Query} from "react-apollo";
+import {Helmet} from 'react-helmet';
+import Movie from "./Movie";
 import {HOME_PAGE} from "./queries";
+import {useQuery} from "react-apollo-hooks";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.7fr);
+  flex-wrap: wrap;
+  justify-items: center;
+`;
 
 const Home = () => (
-	<Query query={HOME_PAGE}>
-	{({loading, data, error}) => {
-	if(loading) return "loading";
-	if(error) return "error";
-	return data.movies.map(movie =>(
-		<h2 key = {movie.id} >
-			{movie.title} / {movie.rating}
-		</h2>
-	));
-}}
-</Query>
+	const {data, loading, error} = useQuery(HOME_PAGE);
+	return (
+		<Container>
+			<Helmet>
+				<title>Home | MovieQL </title>
+			</Helmet>
+			{loading && "Loading"}
+			{error && "Error"}
+			{!loading && data && data.movies && data.movies.map(movie =>(
+				<Movie
+					id={movie.id}
+					key={movie.id}
+					poster={movie.medium_cover_image}
+					title={movie.title}
+					rating={movie.rating}
+				/>
+			))}
+		</Container>
+	);
 );
 
 export default Home;
