d553adbf58ac4f49ceb09442f1cd81897bb14565
양효령/미디어클라우드스쿼드 <hyoryeongyang@sk.com>
2019-06-12 16:43:00 +0900

    modify

--- src/Movie.js
+++ src/Movie.js
@@ -37,7 +37,7 @@ const Movie = ({id,title,rating,poster})=>(
 	</Link>
 );
 
-Movie.proptypes = {
+Movie.propTypes = {
 	id:PropTypes.number.isRequired,
 	title:PropTypes.string.isRequired,
 	rating:PropTypes.number.isRequired,
